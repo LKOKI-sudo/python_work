2c8741dff1aceef83e9add93b99ac713990d4105
LKOKI-sudo <lkoki2016@agruni.edu.ge>
2025-09-11 22:42:28 -0400

    Merge remote changes

---
+++ chapter_3/intentional_error.py
@@ -0,0 +1,7 @@
+names = [ "Leri" , "Luka" , "beqa" , "Nika" , "Misho" , "ladi" , "mariam" , "ana" , "nino" , "sandro" ]
+print (f" these are the names of my friends: {names[0]} , {names[1]} , {names[2].title()} , {names[3]} , {names[4]} , {names[5].title()} , {names[6].title()} , {names[7].title()} , {names[8].title()} , {names[9].title()}")
+# in this code I made an intentional error
+# I will try to access an index that is out of range
+# this will raise an IndexError
+# Uncomment the line below to see the error
+# print ({names[-11]})
