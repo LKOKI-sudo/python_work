1377437c60fad12d2d42531b27b754e34c3e9631
LKOKI-sudo <lkoki2016@agruni.edu.ge>
2025-09-13 23:26:31 -0400

    Merge remote changes

---
+++ chapter_4/players.py
@@ -0,0 +1,10 @@
+# chapter 4 / players.py
+# Created by: LKOKI-SUDO
+# Date: 2024-06-18
+# Description: A simple program to write comprehensions that generate a list of players.
+
+players = [ 'charles' , "martina" , "michael" , "florence" , "eli"]
+print (players[0:3])
+print (players[1:4])
+print (players[:4])
+print (players[2:])
